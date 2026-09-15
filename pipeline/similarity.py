c5d0842b0e7e3cb5699b45c4e944d9826bc2e6e2
Navadeep Jakkamsetti <navadeepjakkamsetti26@gmail.com>
2026-09-15 12:08:37 +0530

    Refactored commit

--- pipeline/similarity.py
+++ pipeline/similarity.py
@@ -1,15 +1,15 @@
 from sentence_transformers import SentenceTransformer, util
 
-_sbert_model = None
+_model = None
 
-def get_sbert_model():
-    global _sbert_model
-    if _sbert_model is None:
-        _sbert_model = SentenceTransformer("all-MiniLM-L6-v2")
-    return _sbert_model
+def get_model():
+    global _model
+    if _model is None:
+        _model = SentenceTransformer("all-MiniLM-L6-v2")
+    return _model
 
 def compute_semantic_score(resume_text, jd_text):
-    model = get_sbert_model()
+    model = get_model()
     r_emb = model.encode(resume_text, convert_to_tensor=True)
     j_emb = model.encode(jd_text, convert_to_tensor=True)
     return round(util.cos_sim(r_emb, j_emb).item() * 100, 1)
